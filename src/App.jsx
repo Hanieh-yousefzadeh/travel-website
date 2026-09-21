73995f0826e53418a41c93011a352d4153618811
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-21 23:41:09 +0330

    fix : fetch and add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,81 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router"
 import Home from "./pages/home";
 import Weather from "./pages/weather";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 import MyTrip from "./pages/mytrip";
+import { CountryContext } from "./context/countrycontext";
+
+const apikey = import.meta.env.VITE_COUNTRIES_API_KEY;
+// console.log(apikey);
+
 
 function App() {
 
+  const [countries, setCountries] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
 
+  useEffect(() => {
+    async function getCountry() {
+      setLoading(true)
+      setError("")
+      try {
+        let allCountries = [];
+        let offset = 0;
+        let more = true;
+        while (more) {
+          const response = await fetch(`https://api.restcountries.com/countries/v5?limit=100&offset=${offset}`, {
+            headers: {
+              'Authorization': `Bearer ${apikey}`
+            }
+          }); // console.log(respone)
+          if (!response.ok) {
+            throw new Error("Something went wrong")
+          }
+          const data = await response.json();
+          // console.log(data)
+          const pageCountries = data.data.objects;
+
+          allCountries = [...allCountries, ...pageCountries]
+
+          more = data.data.meta.more;
+          offset += 100
+        }
+
+        setCountries(allCountries)
+
+      } catch (error) {
+        setError("Something went wrong. Please try again.")
+        setCountries([])
+      } finally {
+        setLoading(false)
+      }
+
+    }
+    getCountry()
+
+  }, [])
+
+  if (loading) {
+    return (<h1>loading ...</h1>)
+  }
+
+  if (error) {
+    return (<h1>{error}</h1>)
+  }
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/country/:id" element={<Weather />} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/favorite" element={<MyTrip/>} />
-      </Routes>
+      <CountryContext value={{ countries }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/country/:id" element={<Weather />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/favorite" element={<MyTrip />} />
+        </Routes>
+      </CountryContext>
 
     </>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,71 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Link } from "react-router";
-
-
-const apikey = import.meta.env.VITE_COUNTRIES_API_KEY;
-// console.log(apikey);
-
+import { CountryContext } from "../context/countrycontext";
 
 function Home() {
-    const [countries, setCountries] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState("")
-
-    useEffect(() => {
-        async function getCountry() {
-            setLoading(true)
-            setError("")
-            try {
-                let allCountries = [];
-                let offset = 0;
-                let more = true;
-                while (more) {
-                    const response = await fetch(`https://api.restcountries.com/countries/v5?limit=100&offset=${offset}`, {
-                        headers: {
-                            'Authorization': `Bearer ${apikey}`
-                        }
-                    }); // console.log(respone)
-                    if (!response.ok) {
-                        throw new Error("Something went wrong")
-                    }
-                    const data = await response.json();
-                    console.log(data)
-                    const pageCountries = data.data.objects;
-
-                    allCountries = [...allCountries, ...pageCountries]
-
-                    more = data.data.meta.more;
-                    offset += 100
-                }
-
-                setCountries(allCountries)
-
-            } catch (error) {
-                setError("Something went wrong. Please try again.")
-                setCountries([])
-            } finally {
-                setLoading(false)
-            }
-
-        }
-        getCountry()
-
-    }, [])
-
-    if (loading) {
-        return (<h1>loading ...</h1>)
-    }
-
-    if (error) {
-        return (<h1>{error}</h1>)
-    }
-
+    const { countries } = useContext(CountryContext)
     return (
         <div>
             {countries.map((country) => (
-                <Link to="/country/:id">
-                    <img src={country.flag.url_png} alt="" />
-                    <h1 key={country.uuid}>{country.names.common}</h1>
+                <Link key={country.uuid} to={`/country/${country.uuid}`}>
+                    {country.flag.url_svg !== "" && (<img src={country.flag.url_svg} />)}
+                    <h1 >{country.names.common}</h1>
                 </Link>
             )
             )}
