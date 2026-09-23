bc24fd4dea1201a61f3f48ddb9290b5e113e49bd
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-23 08:01:59 +0330

    feat : add login and add AuthContext

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,26 @@
-import { Link } from "react-router";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router";
+import { AuthContext } from "../context/authcontect";
 
-function Header(){
-    return(
+function Header() {
+
+    const navigate = useNavigate();
+
+    const { user, setUser } = useContext(AuthContext)
+    function handelLog() {
+        setUser(null)
+        localStorage.removeItem("user")
+        navigate("/login")
+    }
+    return (
         <nav>
-                <Link to="/favorite"> favorite</Link>
+            <Link to ="/">tripy</Link>
+            <Link to="/favorite"> favorite</Link>
+            {!user && (<Link to="/login"> Login</Link>)}
+            {user && (<>
+            <Link to="/profile">Account Center</Link>
+                <button onClick={handelLog}>logout</button>
+            </>)}
         </nav>
     )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,113 @@
-function Login(){
-    return(
-        <h1>
-            login page
-        </h1>
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router";
+import { AuthContext } from "../context/authcontect";
+
+
+function Login() {
+
+    const navigate = useNavigate()
+
+    const [name, setName] = useState("")
+    const [family, setFamily] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const [error, setError] = useState("")
+
+    const { setUser } = useContext(AuthContext);
+
+    const correctEmail = "royaltrip@gmail.com"
+    const correctPassword = "14725869"
+
+
+
+    function handelSubmit(e) {
+
+        e.preventDefault();
+
+        if (name.trim() === "") {
+            setError("please enter name");
+            return
+        }
+        if (family.trim() === "") {
+            setError("please enter family");
+            return
+        }
+        if (email.trim() === "") {
+            setError("please enter email");
+            return
+        }
+        if (email !== correctEmail) {
+            setError("email invalid");
+            return
+        }
+        if (password.trim() === "") {
+            setError("please enter password");
+            return
+        }
+        if (password !== correctPassword) {
+            setError("password invalid");
+            return
+        }
+        if (confirmPassword.trim() === "") {
+            setError("please enter confirmPassword");
+            return
+        }
+
+        if (confirmPassword !== password) {
+            setError("passwords do not match");
+            return
+        }
+
+        setError("");
+
+        setUser({
+            name: name,
+            family: family,
+            email: email
+        })
+        localStorage.setItem("user", JSON.stringify({ name: name, family: family, email: email }));
+
+        navigate("/")
+    }
+
+    function handelName(e) {
+        setName(e.target.value)
+    }
+
+    function handelFamily(e) {
+        setFamily(e.target.value)
+    }
+
+    function handelEmail(e) {
+        setEmail(e.target.value)
+    }
+
+    function handelPassword(e) {
+        setPassword(e.target.value)
+    }
+    function handelConfirmPassword(e) {
+        setConfirmPassword(e.target.value)
+    }
+
+
+
+    return (
+        <div>
+            <h1>login page</h1>
+            <form onSubmit={handelSubmit}>
+                <p>{error}</p>
+                <input type="text" value={name} placeholder="name" onChange={handelName} />
+                <input type="text" value={family} placeholder="family" onChange={handelFamily} />
+                <input type="email" value={email} placeholder="email" onChange={handelEmail} />
+                <input type="password" value={password} placeholder="password" onChange={handelPassword} />
+                <input type="password" value={confirmPassword} placeholder="confirm password" onChange={handelConfirmPassword} />
+
+                <button type="submit">Login</button>
+
+            </form>
+        </div>
     )
 }
 export default Login;
