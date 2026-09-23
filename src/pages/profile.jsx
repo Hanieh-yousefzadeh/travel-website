f1fae04db278865901231873c0200ad7f2f23dec
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-23 12:08:32 +0330

    feat : add profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,6 +1,18 @@
-function Profile(){
-    return(
-        <h1>my profile</h1>
+import { useContext } from "react";
+import { AuthContext } from "../context/authcontect";
+
+function Profile() {
+    const { user } = useContext(AuthContext)
+    console.log(user);
+
+    return (
+        <div>
+            <h1>my profile</h1>
+            <h3>{user.name}</h3>
+            <h3>{user.family}</h3>
+            <h3>{user.email}</h3>
+        </div>
+
     )
 }
 export default Profile;
