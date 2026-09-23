77c67589cb13ee5117e083d755e1d021fbe0fe63
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-23 10:54:13 +0330

    feat : add search country

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router";
 import { CountryContext } from "../context/countrycontext";
 
 function Home() {
     const { countries } = useContext(CountryContext)
+    const [search , setSearch] = useState("")
+
+    const searchedCountries = countries.filter((country) =>(
+        country.names.common.toLowerCase().includes(search.toLowerCase())
+    ))
+
+    function handleSearch(e){
+        setSearch(e.target.value)
+    }
+
+    function deleteSearch(){
+        setSearch("")
+    }
     return (
         <div>
-            {countries.map((country) => (
+            <input type="text" value={search} onChange={handleSearch}  className="border"/>
+
+            {search && (
+                <button onClick={deleteSearch}>x</button>
+            )}
+
+
+
+            {(search ? searchedCountries : countries).map((country) => (
                 <Link key={country.uuid} to={`/country/${country.uuid}`}>
                     {country.flag.url_svg !== "" && (<img src={country.flag.url_svg} />)}
                     <h1 >{country.names.common}</h1>
