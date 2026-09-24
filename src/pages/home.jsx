577ec05879a904e7f9aa6d40536a410e2e1576f3
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-24 17:47:01 +0330

    feat :style header and hero and add hambergur menu

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,39 +1,44 @@
 import { useContext, useState } from "react";
 import { Link } from "react-router";
 import { CountryContext } from "../context/countrycontext";
+import Hero from "../components/hero";
 
 function Home() {
     const { countries } = useContext(CountryContext)
-    const [search , setSearch] = useState("")
+    const [search, setSearch] = useState("")
 
-    const searchedCountries = countries.filter((country) =>(
+    const searchedCountries = countries.filter((country) => (
         country.name.toLowerCase().includes(search.toLowerCase())
     ))
 
-    function handleSearch(e){
+    function handleSearch(e) {
         setSearch(e.target.value)
     }
 
-    function deleteSearch(){
+    function deleteSearch() {
         setSearch("")
     }
     return (
         <div>
-            <input type="text" value={search} onChange={handleSearch}  className="border"/>
+            <Hero />
+            <section className="">
+                <input type="text" value={search} onChange={handleSearch} className="border" />
 
-            {search && (
-                <button onClick={deleteSearch}>x</button>
-            )}
+                {search && (
+                    <button onClick={deleteSearch}>x</button>
+                )}
 
 
 
-            {(search ? searchedCountries : countries).map((country) => (
-                <Link key={country.numericCode} to={`/country/${country.numericCode}`}>
-                    {country.flags.png !== "" && (<img src={country.flags.png } />)}
-                    <h1 >{country.name}</h1>
-                </Link>
-            )
-            )}
+                {(search ? searchedCountries : countries).map((country) => (
+                    <Link key={country.numericCode} to={`/country/${country.numericCode}`}>
+                        {country.flags.png !== "" && (<img src={country.flags.png} />)}
+                        <h1 >{country.name}</h1>
+                    </Link>
+                )
+                )}
+            </section>
+
         </div>
     )
 } export default Home;
