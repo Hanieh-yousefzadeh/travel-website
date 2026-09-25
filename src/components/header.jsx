9df6d6bbf28f02c2badce5ce5a599d31b5651cf8
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-25 18:46:04 +0330

    feat :style single page login

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,7 +21,7 @@ function Header() {
     return (
         <nav className="relative">
             <div className=" bg-[#EBE6D4]  sm:h-17 h-11 w-full sm:px-10 px-5 flex items-center justify-between text-[#26141A]">
-                <Link to="/" className=" xl:text-3xl text-xl font-bold flex items-baseline"><MapPinned className="size-5"/>Tripy</Link>
+                <Link to="/" className=" xl:text-3xl text-xl font-bold flex items-baseline"><MapPinned className="size-5 text-[#5A392D]"/>Tripy</Link>
                 <button onClick={handelClick} className="sm:hidden ">
                     {isOpen ? (<X className="p-0.5 w-7 h-6 sm:w-9 sm:h-8 rounded-sm text-[#26141A]" />)
                         :
@@ -29,12 +29,12 @@ function Header() {
                     }
                 </button>
                 <div className="sm:flex lg:gap-10 gap-5 text-base font-medium items-center hidden">
-                    <Link to="/favorite" className=" flex items-center gap-1 hover:rounded-full hover:bg-amber-50 px-2 py-2"><Heart className="lg:size-7  p-1 rounded-full border-2  hover:fill-[#26141A]" /> favorite</Link>
-                    {!user && (<Link to="/login" className="rounded-full bg-amber-50 pt-2 pb-3 px-7 hover:bg-[#26141A] hover:text-amber-50"> Login</Link>)}
+                    <Link to="/favorite" className=" flex items-center gap-1 hover:rounded-full hover:bg-amber-50 px-2 py-2"><Heart className="lg:size-7  p-1 rounded-full border-2 text-[#5A392D] hover:fill-[#5A392D]" /> favorite</Link>
+                    {!user && (<Link to="/login" className="rounded-full bg-amber-50 pt-2 pb-3 px-7 hover:bg-[#5A392D] hover:text-amber-50"> Login</Link>)}
                     {user && (
                         <div className="flex lg:gap-10 gap-5">
-                            <Link to="/profile" className="flex gap-1 items-center hover:rounded-full hover:bg-amber-50 px-2 py-2"><UserRound className="lg:size-7  p-1 rounded-full border-2   hover:fill-[#26141A]" strokeWidth={2} />My Account</Link>
-                            <button onClick={handelLog} className="rounded-full bg-amber-50 sm:pt-1 sm:pb-2 sm:px-5 hover:bg-[#26141A] hover:text-amber-50">Logout</button>
+                            <Link to="/profile" className="flex gap-1 items-center hover:rounded-full hover:bg-amber-50 px-2 py-2"><UserRound className="lg:size-7  p-1 rounded-full text-[#5A392D] border-2 hover:fill-[#5A392D]" strokeWidth={2} />My Account</Link>
+                            <button onClick={handelLog} className="rounded-full bg-amber-50 sm:pt-1 sm:pb-2 sm:px-5  hover:bg-[#5A392D] hover:text-amber-50">Logout</button>
                         </div>)}
                 </div>
             </div >
